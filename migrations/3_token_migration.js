cb9f30fffcb3bb9d38b902f0316041573aecaee2
Alex Johnson <ajohnson2389@gmail.com>
2022-02-14 20:09:31 -0500

    updated for cleaner testing with truffle

--- migrations/3_token_migration.js
+++ migrations/3_token_migration.js
@@ -1,5 +1,16 @@
 const testLink = artifacts.require('testLink');
+const Wallet = artifacts.require('Wallet');
 
-module.exports = function (deployer) {
-    deployer.deploy(testLink);
+module.exports = async function (deployer) {
+    await deployer.deploy(testLink);
+    let wallet = await Wallet.deployed();
+    let tlink = await testLink.deployed();
+    await tlink.approve(wallet.address, 500);
+    await wallet.addToken(web3.utils.fromUtf8('tLINK'), tlink.address);
+    await wallet.deposit(100, web3.utils.fromUtf8('tLINK'));
+    let balance_of_tlink = await wallet.balances(
+        accounts[0],
+        web3.utils.fromUtf8('tLINK')
+    );
+    console.log(balance_of_tlink);
 };
